441c9ac3895d1a8ef1b74a29d6f4119eaa4bd5a6
MetatransApps <kitopchiyski@gmail.com>
2022-10-01 19:01:43 +0300

    Fix default backgroundcolor by MODEL_VERSION in UserSettings object

--- app/src/main/java/org/metatrans/apps/gravity/model/UserSettings_Gravity.java
+++ app/src/main/java/org/metatrans/apps/gravity/model/UserSettings_Gravity.java
@@ -10,12 +10,17 @@ import org.metatrans.commons.graphics2d.model.UserSettings;
 public class UserSettings_Gravity extends UserSettings {
 	
 	
-	private static final long serialVersionUID = 3199714911195754477L;
-	
+	private static final long serialVersionUID 		= 3199714911195754477L;
+
+	public static final int MODEL_VERSION_1 		= 0;
+	public static final int MODEL_VERSION_2 		= 1;
+
 	private static final int DEFAULT_CFG_ID_COLOUR = IConfigurationColours.CFG_COLOUR_BLUE_PETROL;
 
 
 	public int cfg_id_space_objects;
+
+	public int model_version = MODEL_VERSION_2;
 
 
 	public UserSettings_Gravity() {
@@ -31,18 +36,20 @@ public class UserSettings_Gravity extends UserSettings {
 	@Override
 	protected void fixFields(String op) {
 
-		if (cfg_id_space_objects == 0) {
+		if (cfg_id_space_objects == 0 || model_version == MODEL_VERSION_1) {
 
 			cfg_id_space_objects = ConfigurationUtils_SpaceObjects.START_INDEX;
 		}
 
-		if (uiColoursID == 0) {
+		if (uiColoursID == 0 || model_version == MODEL_VERSION_1) {
 	    	uiColoursID 		= DEFAULT_CFG_ID_COLOUR;
 	    	System.out.println("UserSettings: " + op + " - updating colour id");
 	    }
 	    
-		if (modeID == 0) {
+		if (modeID == 0 || model_version == MODEL_VERSION_1) {
 			modeID = ConfigurationUtils_Level.LEVEL_ID_DEFAULT;
 		}
+
+		model_version = MODEL_VERSION_2;
 	}
 }
